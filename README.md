721da0a8d1fafbeeb75daf35d9cba990927eaf21
RuiDTLima <rui_lima24@hotmail.com>
2018-04-04 23:47:15 +0100

    Trying to get access to pom file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Build Status](https://travis-ci.org/pt-osda/java-dependencies-analysis.svg?branch=master)](https://travis-ci.org/pt-osda/java-dependencies-analysis)
+[![license](https://img.shields.io/github/license/pt-osda/java-dependencies-analysis.svg)](https://github.com/pt-osda/java-dependencies-analysis/blob/master/LICENSE)
+
 # java-dependencies-analysis
 Analyse Open Source dependencies in projects development. Aplication to analyse security and license aspects in Open Source project's dependencies, applicable to Java platform.
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,20 @@
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.*;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.logging.Logger;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Set;
+import org.gradle.internal.impldep.org.apache.maven.model.Model;
+import org.gradle.internal.impldep.org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.gradle.internal.impldep.org.apache.maven.project.MavenProject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HelloWorld implements Plugin<Project> {
+    private final String MavenCentralRepositoryUrl = "https://search.maven.org/remotecontent?filepath=";
     @Override
     public void apply(Project project) {
         Logger logger = project.getLogger();
@@ -34,18 +40,65 @@ public class HelloWorld implements Plugin<Project> {
             e.printStackTrace();
         }
 
+/*        ArtifactResolutionResult artifactResolutionResult = project.getDependencies()
+                                                            .createArtifactResolutionQuery()
+                                                            .forComponents()
+                                                            .withArtifacts(MavenModule.class, MavenPomArtifact.class).execute();
 
+        logger.info("Start Components analyse");
+
+        for (ComponentArtifactsResult component: artifactResolutionResult.getResolvedComponents()) {
+            component.getArtifacts(MavenPomArtifact.class).forEach((element) -> logger.info(String.format("POM file for %s: %s", component.getId(), element.getId())));
+        }
+
+        logger.info("Finish Components analyse");
+*/
         ConfigurationContainer configurationContainer = project.getConfigurations();
-
         Configuration configuration = configurationContainer.getByName("compile");
 
-        //configuration.
         DependencySet dependencySet = configuration.getDependencies();
 
         for (Dependency dependency : dependencySet) {
-            logger.info(String.format("Compile Configuration Dependency %s", dependency.getName()));
+            logger.info(String.format("Compile Configuration Dependency name=%s, group=%s, version=%s", dependency.getName(), dependency.getGroup(), dependency.getVersion()));
+            String dependencyURL = produceURL(dependency.getName(), dependency.getGroup(), dependency.getVersion());
+            logger.info(dependencyURL);
+
+            try {
+                URL url = new URL(dependencyURL);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+
+
+                Model model = null;
+                FileReader reader = null;
+                MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+                try {
+                    reader = new FileReader(String.valueOf(con.getInputStream()));
+                    model = mavenreader.read(reader);
+                    //model.setPomFile(pomfile);
+                }catch(Exception ex){}
+
+                ObjectInputStream objectInputStream = new ObjectInputStream(con.getInputStream());
+                model.Project receivedProject = (model.Project) objectInputStream.readObject();
+                /*BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();*/
+                logger.info("Http Response: " + receivedProject);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             //Project
             //dependency.get
         }
+    }
+
+    private String produceURL(String name, String group, String version){
+        String urlgroup = group.replaceAll("\\.", "/");
+        return MavenCentralRepositoryUrl + urlgroup + "/" + name + "/" + version + "/" + name + "-" + version + ".pom";
     }
 }
